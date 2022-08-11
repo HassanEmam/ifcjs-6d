f59dacc63a49a79c9c24204fa2696b3846eee9d0
Badr OUAHBI <badii_00@hotmail.com>
2022-08-11 23:33:14 +0200

    Index.html page

---
+++ javascript/index.js
@@ -0,0 +1,39 @@
+import { projectsCards } from "./projects.js";
+
+const projectsContainer = document.getElementById("projectsContainer");
+const projectsContainerChildren = projectsContainer.children;
+const projectsContainerChildrenArray = Array.from(projectsContainerChildren);
+
+// Get the template card
+const templateProjectCard = projectsContainerChildrenArray[0];
+
+const baseURL = './viewer.html';
+
+// Copy the project Card and paste it n times
+for (let project of projectsCards) {
+    // Create a new card
+    const newCard = templateProjectCard.cloneNode(true);
+
+    // Add project name to the card
+    const cardTitle = newCard.querySelector('.ProjectTitle');
+    cardTitle.textContent = project.name;
+
+    const cardButton = newCard.querySelector('.projectButton');
+
+    //Send value from page to another using URL Parameters
+    let destinationURL = baseURL + `?id=${project.id}&projectURL=${project.url}`;
+
+    // Replace the html href
+    cardButton.href = destinationURL;
+
+    // Replace the html project-id
+    const cardButtonChildren = cardButton.children;
+    const projectImage = newCard.querySelector('.imageCard');
+    projectImage.src = `./asset/${project.id}.jpeg`;
+    
+    // Add the cards to the DOM (HTML)
+    projectsContainer.appendChild(newCard);
+}
+
+// Remove the template card
+templateProjectCard.remove();
---
+++ javascript/projects.js
@@ -0,0 +1,42 @@
+export const projectsCards = [
+    {
+        name: "Project nº1",
+        id: "pj-1",
+        url: "./IFC/01.ifc"
+    },
+    {
+        name: "Project nº2",
+        id: "pj-2",
+        url: "./IFC/02.ifc"
+    },
+    {
+        name: "Project nº3",
+        id: "pj-3",
+        url: "./IFC/03.ifc"
+    },
+    {
+        name: "Project nº4",
+        id: "pj-4",
+        url: "./IFC/04.ifc"
+    },
+    {
+        name: "Project nº5",
+        id: "pj-5",
+        url: "./IFC/05.ifc"
+    },
+    {
+        name: "Project nº6",
+        id: "pj-6",
+        url: "./IFC/Duplex_A.ifc"
+    },
+    {
+        name: "Project nº7",
+        id: "pj-7",
+        url: "./IFC/Clinic_Structural.ifc"
+    },
+    {
+        name: "Project nº8",
+        id: "pj-8",
+        url: "./IFC/IFC Schependomlaan.ifc"
+    }
+];
