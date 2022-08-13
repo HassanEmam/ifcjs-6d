4fb0f4cc86d991fd7ff60a9a4addf6f19388f8ec
HassanEmam <hassan.emam@hotmail.com>
2022-08-13 12:21:21 +0100

    Update projects.js

--- projects.js
+++ projects.js
@@ -2,26 +2,26 @@ export const projects = [
   {
     name: "Model 1 A",
     id: "01",
-    url: "../ifc/01.ifc",
+    url: "./ifc/01.ifc",
   },
   {
     name: "Model 2",
     id: "02",
-    url: "../ifc/02.ifc",
+    url: "./ifc/02.ifc",
   },
   {
     name: "Model 3",
     id: "03",
-    url: "../ifc/03.ifc",
+    url: "./ifc/03.ifc",
   },
   {
     name: "Model 4",
     id: "04",
-    url: "../ifc/04.ifc",
+    url: "./ifc/04.ifc",
   },
   {
     name: "Model 5",
     id: "05",
-    url: "../ifc/05.ifc",
+    url: "./ifc/05.ifc",
   },
 ];
