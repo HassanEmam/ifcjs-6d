d0498038b5d467ce78c81da73c548acde03d6b0c
Badr OUAHBI <badii_00@hotmail.com>
2022-08-28 19:13:29 +0200

    add model and correct descriptions

--- projects.js
+++ projects.js
@@ -8,51 +8,60 @@ export const projects = [
   //   name: "Model 1",
   //   id: "01",
   //   url: "./ifc/01.ifc",
+  //   description: ""
   // },
   // {
   //   name: "Model 2",
   //   id: "02",
   //   url: "./ifc/02.ifc",
+  //   description: ""
   // },
   // {
   //   name: "Model 3",
   //   id: "03",
   //   url: "./ifc/03.ifc",
+  //   description: ""
   // },
   // {
   //   name: "Model 4",
   //   id: "04",
   //   url: "./ifc/04.ifc",
+  //   description: ""
   // },
-  // {
-  //   name: "Model 5",
-  //   id: "05",
-  //   url: "./ifc/05.ifc",
-  // },
-
   // {
   //   name: "Model 7",
   //   id: "07",
-  //   url: "./ifc/07.ifc",
+  //   url: "./ifc/07.ifc"
+  //   description: ""
   // },
   // {
   //   name: "Small Shed",
   //   id: "08",
-  //   url: "./ifc/08.ifc",
+  //   url: "./ifc/08.ifc"
+  //   description: ""
   // },
   {
     name: "Nice Cottage",
     id: "09",
     url: "./ifc/09.ifc",
+    description: "This IFC Model has quantity values. You can simulate the carbon footprint of its objects."
   },
   {
     name: "Three-floored Office",
     id: "10",
     url: "./ifc/10.ifc",
+    description: "This IFC Model has quantity values. You can simulate the carbon footprint of its objects."
   },
   {
     name: "Meeting Point",
     id: "11",
     url: "./ifc/11.ifc",
+    description: "This IFC Model has quantity values. You can simulate the carbon footprint of its objects."
+  },
+  {
+    name: "House by Karlsruhe Institute of Technology",
+    id: "12",
+    url: "./ifc/12.ifc",
+    description: "This IFC Model has quantity values. You can simulate the carbon footprint of its objects."
   },
 ];
